d269fad298cb4dd23a02c96090877dfae2fe8c9f
schnipdip <cjh30@pct.edu>
2020-08-24 18:27:58 -0400

    Add files via upload

---
+++ asset.py
@@ -0,0 +1,25 @@
+class buildAsset:
+    def __init__(self, name, license, quantity):
+        self.name       = str(name)
+        self.license    = str(license)
+        self.quantity   = int(quantity)
+    
+    def get_name(self):
+        # name of the Licensed Product
+        return(self.name)
+
+    def get_license(self):
+        return (self.license)
+
+    def get_quantity(self):
+        return(self.quantity)
+
+
+class assignAsset:
+    def __init__(self, hostname):
+        self.hostname   = str(hostname)
+
+    def get_hostname(self):
+        # Endpoint hostname the license is assigned to
+        return(self.hostname)
+
