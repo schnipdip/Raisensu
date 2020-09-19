b3f3c7c1ab8adaa5abbe892d12393ee21843b2a3
schnipdip <cjh30@pct.edu>
2020-09-18 22:53:26 -0400

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -7,20 +7,36 @@ mkdir /opt/Raisensu
 
 #Move Raisensu files to /usr/local/bin
 cp -R ../../Raisensu /opt/
+echo "Moved Raisensu to /opt/Raisensu"
 
 #Copy raisensu_monitor.serivce file to /lib/systemd/system/
 cp raisensu_monitor.service /lib/systemd/system/raisensu_monitor.service
+echo "Copied the raisensu_monitor.service to systemd"
 
 #Change permissions for raisensu_monitor.serivce file
 chmod 644 /lib/systemd/system/raisensu_monitor.service
+echo "Modified the permissions on raisensu_monitor.service"
+
+#Copy raisensu_timer.serivce file to /lib/systemd/system/
+cp raisensu_timer.service /lib/systemd/system/raisensu_timer.service
+echo "Copied the raisensu_timer.service to systemd"
+
+#Change permissions for raisensu_timer.serivce file
+chmod 644 /lib/systemd/system/raisensu_time.service
+echo "Modified the permissions on raisensu_timer.service"
 
 #Restart systemd daemon
+echo "Restarting systemd daemon"
 systemctl daemon-reload
+echo "Systemd daemon reloaded"
 
 #Remove Raisensu/windows directory
 rm -r /opt/Raisensu/windows
+echo "Removed /opt/Raisensu/windows directory"
 
 #Echo new access location
+echo ""
+echo ""
 echo "Raisensu has been installed in /opt/Raisensu"
 echo ""
 echo "Change Directories to /opt/Raisensu/linux/"
