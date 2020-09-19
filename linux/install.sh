58bd6431885717f6a75f2945a07994993cb3a205
schnipdip <cjh30@pct.edu>
2020-09-18 22:36:58 -0400

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -17,14 +17,14 @@ chmod 644 /lib/systemd/system/raisensu_monitor.service
 #Restart systemd daemon
 systemctl daemon-reload
 
-#Start raisensu_monitor.service
-systemctl start raisensu_monitor.service
-
-#Enable raisensu_monitor.service to start at start-up
-systemctl enable raisensu_monitor.service
-
 #Remove Raisensu/windows directory
 rm -r /opt/Raisensu/windows
 
 #Echo new access location
-echo "Raisensu has been installed in /opt/Raisensu"
+echo "Raisensu has been installed in /opt/Raisensu\n"
+echo "Move to /opt/Raisensu/linux/\n"
+echo "Perform the following steps in this order:\n"
+echo "sudo python3 generate_key\n"
+echo "sudo python3 raisensu.py -t\n"
+echo "sudo systemctl start raisensu_monitor.service\n"
+echo "systemctl enable raisensu_monitor.service"
